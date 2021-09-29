e197886e12f4c39770fe8a8c2ed04bfae82cb853
unknown <montasirazad@gmail.com>
2021-09-30 02:10:40 +0600

    contex

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import Shipment from './Components/Shipment/Shipment';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contex API</h1>
+      <Home></Home>
+      <Header></Header>
+      <Shipment></Shipment>
     </div>
   );
 }

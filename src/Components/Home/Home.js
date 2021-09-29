dc48d236d5d1e3d37367c9acc0a574c83c2af78f
unknown <montasirazad@gmail.com>
2021-09-30 02:38:24 +0600

    share data with child compo

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import Categories from '../categories/Categories';
+ 
 const Home = (props) => {
     const {count} = props
     return (
-        <div>
+        <div style={{border:"2px solid purple",margin:'10px', padding:'10px' }}>
             <h1>This is Home : {count}</h1>
-        </div>
+            <Categories count={count}></Categories>
+         </div>
     );
 };
 

b8ab8559ab9693d3ea1bfb01102a1166da9d844a
unknown <montasirazad@gmail.com>
2021-09-30 02:23:23 +0600

    parent level state share

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 import Home from './Components/Home/Home';
 import Header from './Components/Header/Header';
 import Shipment from './Components/Shipment/Shipment';
 
 function App() {
+  const [count , setCount] = useState(0)
   return (
     <div className="App">
       <h1>Contex API</h1>
-      <Home></Home>
-      <Header></Header>
+      <p>count value = {count}</p>
+      <Header count={count} setCount={setCount}></Header>
+      <Home  count={count}></Home>
       <Shipment></Shipment>
     </div>
   );
